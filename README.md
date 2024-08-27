54f61770b2e73ff647fa9d2c139be34ccc2c7ac7
Tue Skeltved <bigheadedmonster@users.noreply.github.com>
2024-08-27 13:12:25 +0200

    wrapping up version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# signaturgruppen-age-verification-demo
-Signaturgruppen age verification demo
+# Signaturgruppen Age Verification demo
+
+This example demonstrates a solution in pure vanilla JavaScript that integrates to Signaturgruppen Age Verification.
+
+Setup for a predefined PP environment client, run the folder as a webapplication
+
+See the sg-age-verify-config.js file for configuration parameters such as the age to verify and client id.
+
+## MitID testusers. 
+To test with MitID testusers in the PP environment, use the official MitID testtool: https://pp.mitid.dk/test-tool/frontend/#/create-identity.
--- sg-age-verify.js
+++ sg-age-verify.js
@@ -1,29 +1,49 @@
-function sgBrokerAgeResultInlineJwtParseValidation(ageToVerify, result){
-    function parseJwt (token) {
-        var base64Url = token.split('.')[1];
-        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        }).join(''));
-    
-        return JSON.parse(jsonPayload);
-    }
+function sgBrokerOnlineValidateIdToken(idToken, resolve)
+{
+    //sg_broker_id_token_endpoint -> see swagger endpoint at: https://pp.netseidbroker.dk/op/swagger/index.html
+    //sg_broker_id_token_endpoint validates Signaturgruppen ID token. It is recommended to call this from your backend. 
+    //if invoked from the browser, as from this demo, end-users might be able to circumvent the JS code and affect the result.
+    var bodyData = { idToken : idToken };
+    fetch(this.sg_broker_id_token_endpoint, {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(bodyData)
+    }).then(res => res.json())
+    .then(jsonResponse => {
+        resolve({ age_to_verify: this.sg_broker_demo_age_to_verify, age_verified: jsonResponse.validated, claims : jsonResponse.claims });
+    });
+}
+
+function sgBrokerHandleResponse(event, resolve){
+    var result = JSON.parse(event.data.result);
 
     if(typeof result.id_token !== 'undefined'){
-        var parsedToken = parseJwt(result.id_token);
-        var ageClaim = parsedToken.idbrokerdk_age_verified;
-        var ageSplitted = ageClaim.split(':');
-        var ageVerified = ageSplitted[1] == 'true';
-        var ageToVerifyFromClaim = parseInt(ageSplitted[0]);
-       return { age_to_verify: ageToVerify, age_verified: ageVerified && ageToVerifyFromClaim == ageToVerify };
+        sgBrokerOnlineValidateIdToken(result.id_token, resolve);
     }else{
-        return { age_to_verify: ageToVerify, age_verified: false, error_description : result.error_description };
+        resolve({ age_to_verify: this.sg_broker_demo_age_to_verify, age_verified: false, error_description : result.error_description });
     }
 }
 
-function sgBrokerStartAgeVerify(ageToVerify) {
+function sgBrokerIsAgeEvent(event){
+    return  event.origin == this.sg_broker_origin && event.data.command === 'webmessage_flow_response';
+}
+
+function sgBrokerStartAgeVerify(nonce) {
     return new Promise((resolve, reject) => {
         var windowRef;
+        var nonceVerifyResolve = function (result) 
+        {
+            if(result.age_verified == true){
+                if(nonce !== result.claims.nonce){
+                    resolve({ age_to_verify: this.sg_broker_demo_age_to_verify, age_verified: false, error_description : 'nonce validation error' });
+                    return;
+                }
+                resolve(result);
+            }
+            if(result.age_verified == false){
+                resolve(result);
+            }
+        };
         
         function popupWindow(n, t, i, r){
             let u = window.outerHeight / 2 + window.screenY - r / 2
@@ -33,29 +53,26 @@ function sgBrokerStartAgeVerify(ageToVerify) {
             windowRef.focus()
         }
         
-        function showPopup() {
-            var url = `${sg_broker_authorize_endpoint}?client_id=${sg_broker_clientId}&redirect_uri=${sg_broker_redirect_uri}&response_type=id_token&scope=openid%20age_verify:${ageToVerify}&idp_values=idbrokerdk&prompt=login&nonce=nonce1`;
+        function ShowPopup() {
+            var url = `${sg_broker_authorize_endpoint}?client_id=${sg_broker_clientId}&redirect_uri=${sg_broker_redirect_uri}&response_type=id_token&scope=openid%20age_verify:${sg_broker_demo_age_to_verify}&idp_values=idbrokerdk&prompt=login&nonce=${nonce}`;
             var title = 'Verificer Alder';
             var width = '452';
             var height = '640';
-            //popupWindow('https://pp.netseidbroker.dk/op/connect/authorize?client_id=aac81a04-6568-4912-be8b-00ccba7b8b39&redirect_uri=https://localhost:5001/WebMessagingCallback/ImplicitCallback&response_type=id_token&scope=openid%20age_verify:16&idp_values=idbrokerdk&prompt=login&nonce=nonce1', 'Verificer Alder', '452', '640');
             popupWindow(url, title, width, height);
         }
-
+        
         function receiveEvent(event) {
             console.log("Received message from origin " + event.origin);
-            
-            if (event.origin == this.sg_broker_origin && event.data.command === 'webmessage_flow_response') {
+
+            if (sgBrokerIsAgeEvent(event)) {
                 windowRef.close();
-                var resultJson = JSON.parse(event.data.result);
-                var inlineResult = sgBrokerAgeResultInlineJwtParseValidation(ageToVerify, resultJson);
-                resolve(inlineResult);
+                sgBrokerHandleResponse(event, nonceVerifyResolve);
             }
         }
         
         window.addEventListener("message", receiveEvent, false);
         
-        showPopup();
+        ShowPopup();
         
     });
 }
