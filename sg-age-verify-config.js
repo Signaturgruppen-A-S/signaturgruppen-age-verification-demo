6ef697d54edc8990d6c28113b40c11f079564215
Tue Skeltved <bigheadedmonster@users.noreply.github.com>
2024-09-10 10:16:23 +0200

    Removed unused config parameter

--- sg-age-verify-config.js
+++ sg-age-verify-config.js
@@ -1,5 +1,4 @@
 var sg_broker_origin = 'https://pp.netseidbroker.dk';
-var sg_broker_id_token_endpoint = `${sg_broker_origin}/op/api/v1/tokenverify`;
 var sg_broker_redirect_uri = `${sg_broker_origin}/op/WebMessagingCallback/ImplicitCallback`;
 var sg_broker_authorize_endpoint = `${sg_broker_origin}/op/connect/authorize`;
 var sg_broker_clientId = "9d3c7d79-96c4-43bc-8562-f0bf88ef69b8";
