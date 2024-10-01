c6a561b69b153eff0055b58df511f14f25fe9674
Tue Skeltved <bigheadedmonster@users.noreply.github.com>
2024-10-01 09:53:45 +0200

    Popup directly before promise

--- sg-age-verify.js
+++ sg-age-verify.js
@@ -4,7 +4,7 @@ function parseJwt (token) {
     var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
-
+    
     return JSON.parse(jsonPayload);
 }
 
@@ -18,7 +18,7 @@ function sgBrokerParseIdTokenResponse(idToken, resolve)
 
 function sgBrokerHandleResponse(event, resolve){
     var result = JSON.parse(event.data.result);
-
+    
     if(typeof result.id_token !== 'undefined'){
         sgBrokerParseIdTokenResponse(result.id_token, resolve);
     }else{
@@ -31,28 +31,30 @@ function sgBrokerIsAgeEvent(event){
 }
 
 function sgBrokerStartAgeVerify(nonce) {
+    var windowRef;
+    
+    function popupWindow(n, t, i, r){
+        let u = window.outerHeight / 2 + window.screenY - r / 2
+        let f = window.outerWidth / 2 + window.screenX - i / 2
+        windowRef = window.open(n, t, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=' + i + ', height=' + r + ', top=' + u + ', left=' + f)
+        
+        windowRef.focus()
+    }
+    
+    function ShowPopup() {
+        var url = `${sg_broker_authorize_endpoint}?client_id=${sg_broker_clientId}&redirect_uri=${sg_broker_redirect_uri}&response_type=id_token&scope=openid%20age_verify:${sg_broker_demo_age_to_verify}&idp_values=idbrokerdk&prompt=login&nonce=${nonce}`;
+        var title = 'Verificer Alder';
+        var width = '452';
+        var height = '640';
+        popupWindow(url, title, width, height);
+    }
+    
+    ShowPopup();
+
     return new Promise((resolve, reject) => {
-        var windowRef;
-        
-        function popupWindow(n, t, i, r){
-            let u = window.outerHeight / 2 + window.screenY - r / 2
-            let f = window.outerWidth / 2 + window.screenX - i / 2
-            windowRef = window.open(n, t, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=' + i + ', height=' + r + ', top=' + u + ', left=' + f)
-            
-            windowRef.focus()
-        }
-        
-        function ShowPopup() {
-            var url = `${sg_broker_authorize_endpoint}?client_id=${sg_broker_clientId}&redirect_uri=${sg_broker_redirect_uri}&response_type=id_token&scope=openid%20age_verify:${sg_broker_demo_age_to_verify}&idp_values=idbrokerdk&prompt=login&nonce=${nonce}`;
-            var title = 'Verificer Alder';
-            var width = '452';
-            var height = '640';
-            popupWindow(url, title, width, height);
-        }
-        
         function receiveEvent(event) {
             console.log("Received message from origin " + event.origin);
-
+            
             if (sgBrokerIsAgeEvent(event)) {
                 windowRef.close();
                 sgBrokerHandleResponse(event, resolve);
@@ -60,8 +62,6 @@ function sgBrokerStartAgeVerify(nonce) {
         }
         
         window.addEventListener("message", receiveEvent, false);
-        
-        ShowPopup();
         
     });
 }
